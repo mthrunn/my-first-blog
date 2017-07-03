de3d7dcf204a85d01205b40feaae1857e2e28ae9
Fernando <mthrunn@gmail.com>
2017-07-02 22:42:24 -0500

    creado el base.html y extendido {post_detail.html, post_list.html}

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
     url(r'^blog/', include('blog.urls')),
 ]
